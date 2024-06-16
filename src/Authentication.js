f151a6ccc600a5ddec4a8d74a5dee020738be534
Abhinandan Jain <abhinandanbansal123@gmail.com>
2024-06-16 18:16:06 +0530

    login/regiter UI updated

--- src/Authentication.js
+++ src/Authentication.js
@@ -25,27 +25,23 @@ function Authentication() {
 
   return (
     <>
-    {/* routing  */}
+      {/* routing  */}
       <Router basename="/">
         <div>
-          <div>
-            <div>
-              {/* <div className="min-h-screen flex flex-col font-sans font-normal">
+          {/* <div className="min-h-screen flex flex-col font-sans font-normal">
           <div className="flex justify-center flex-col text-left w-full h-full">
             <div className="w-112 mx-auto bg-white shadow-lg p-10 rounded-lg transition-all duration-300"> */}
-              <Routes>
-                <Route
-                  path="/"
-                  element={user ? <Navigate to="/app" /> : <Login />}
-                />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                {/* <Route path="/profile" element={<Profile />} /> */}
-                <Route path="/app" element={<App />} />
-              </Routes>
-              <ToastContainer />
-            </div>
-          </div>
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Navigate to="/app" /> : <Login />}
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* <Route path="/profile" element={<Profile />} /> */}
+            <Route path="/app" element={<App />} />
+          </Routes>
+          <ToastContainer />
         </div>
       </Router>
     </>
--- src/auth/Login.js
+++ src/auth/Login.js
@@ -28,44 +28,49 @@ function Login() {
 
   return (
     <>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <h3>Login</h3>
+      <div className="flex flex-col font-sans font-normal">
+        <div className="mx-auto bg-white shadow-lg p-10 rounded-lg transition-all duration-300">
+          <form onSubmit={handleSubmit}>
+            <h3 className="font-bold">Login</h3>
 
-          <div className="mb-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:border-blue-500"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+            <div className="mb-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="w-full px-4 py-2 border rounded focus:outline-none focus:border-blue-500"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:border-blue-500"
-              placeholder="Enter password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+            <div className="mb-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="w-full px-4 py-2 border rounded focus:outline-none focus:border-blue-500"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-          <div className="grid">
-            <button
-              type="submit"
-              className="px-4 py-2 bg-blue-500 text-white font-semibold rounded">
-              Submit
-            </button>
-          </div>
-          <p className="text-right text-sm text-gray-500 mt-2">
-            New user <a href="/register">Register Here</a>
-          </p>
-          {/* <SignInWithGoogle /> */}
-        </form>
+            <div className="grid">
+              <button
+                type="submit"
+                className="px-4 py-2 bg-blue-500 text-white font-semibold rounded">
+                Submit
+              </button>
+            </div>
+            <p className="text-right text-sm text-gray-500 mt-2">
+              New user{" "}
+              <a href="/register" className="text-blue-500 font-bold">
+                Register Here
+              </a>
+            </p>
+            {/* <SignInWithGoogle /> */}
+          </form>
+        </div>
       </div>
     </>
   );
